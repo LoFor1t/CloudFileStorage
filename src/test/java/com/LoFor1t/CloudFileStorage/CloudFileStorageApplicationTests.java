066ca8523078ba0ba1915e39c21d41c09c9a5137
LoFor1t <vgresnyh@gmail.com>
2023-10-04 13:43:19 +0200

    Initial commit

---
+++ src/test/java/com/LoFor1t/CloudFileStorage/CloudFileStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LoFor1t.CloudFileStorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudFileStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
