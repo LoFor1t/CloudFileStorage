7fcdb2b53b7cc9cd7b5f87bdd517308c1c3e6031
LoFor1t <vgresnyh@gmail.com>
2023-10-04 14:19:00 +0200

    Create entities for Users and Roles tables.

---
+++ src/main/java/com/LoFor1t/CloudFileStorage/entity/User.java
@@ -0,0 +1,38 @@
+package com.LoFor1t.CloudFileStorage.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.ManyToAny;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name="user_roles",
+            joinColumns= {@JoinColumn(name="USER_ID", referencedColumnName = "ID")},
+            inverseJoinColumns = {@JoinColumn(name="ROLE_ID", referencedColumnName = "ID")}
+    )
+    private List<Role> roles = new ArrayList<>();
+}
