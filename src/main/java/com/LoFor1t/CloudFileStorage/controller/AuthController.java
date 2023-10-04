7362cf6014116f1ac1ae930d9b81e024bb879d34
LoFor1t <vgresnyh@gmail.com>
2023-10-04 15:15:38 +0200

    Created get mapping for registerForm, loginForm, usersList. Created post mapping for creating new User from register form.

--- src/main/java/com/LoFor1t/CloudFileStorage/controller/AuthController.java
+++ src/main/java/com/LoFor1t/CloudFileStorage/controller/AuthController.java
@@ -1,13 +1,64 @@
 package com.LoFor1t.CloudFileStorage.controller;
 
+import com.LoFor1t.CloudFileStorage.entity.User;
+import com.LoFor1t.CloudFileStorage.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final UserService UserService;
 
     @GetMapping("/index")
     public String home() {
         return "index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        return "register";
+    }
+
+    @PostMapping("/register/save")
+    public String registration(@Valid @ModelAttribute("user") User user,
+                               BindingResult bindingResult,
+                               Model model) {
+
+        User existingUser = UserService.findByUsername(user.getUsername());
+
+        if (existingUser != null && existingUser.getUsername() != null && !existingUser.getUsername().isEmpty()) {
+            bindingResult.rejectValue("email", null, "There is already an account registered with that username");
+        }
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("user", user);
+            return "register";
+        }
+
+        UserService.saveUser(user);
+
+        return "redirect:/register?success";
+    }
+
+    @GetMapping("/users")
+    public String users(Model model) {
+        model.addAttribute("users", UserService.findAllUsers());
+        return "users";
     }
 }
